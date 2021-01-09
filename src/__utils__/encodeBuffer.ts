81fa063ce7abf76b93c304fce10c99fb1cc5c0fe
Vadym Borodin <borodean@gmail.com>
2021-01-09 16:37:30 +0200

    Enable the "@typescript-eslint/explicit-module-boundary-types" ESLint rule

--- src/__utils__/encodeBuffer.ts
+++ src/__utils__/encodeBuffer.ts
@@ -1,4 +1,4 @@
-export default (buffer: Buffer, mediaType: string) => {
+export default (buffer: Buffer, mediaType: string): string => {
   if (mediaType === "image/svg+xml") {
     return `charset=utf-8,${encodeURIComponent(
       buffer.toString("utf8").trim()
--- src/__utils__/exists.ts
+++ src/__utils__/exists.ts
@@ -3,7 +3,7 @@ import fs from "fs";
 export default (
   filePath: string,
   callback: (err: NodeJS.ErrnoException | null, exists: boolean) => void
-) => {
+): void => {
   fs.stat(filePath, (err) => {
     callback(null, err === null);
   });
