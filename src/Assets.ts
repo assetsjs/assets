0a472a6c3a8f10a0c68d9a820a1a5ee0c3bf426f
Vadym Borodin <borodean@gmail.com>
2021-01-10 02:40:55 +0200

    Extract the Assets class into a separate file

---
+++ src/Assets.ts
@@ -0,0 +1,30 @@
+import url from "url";
+
+import Asset from "./Asset";
+import resolvePath from "./path";
+import { Options } from "./types";
+
+class Assets {
+  constructor(private options: Options = {}) {}
+
+  resolve(path: string): Promise<Asset> {
+    const urlObject = url.parse(path);
+
+    if (!urlObject.pathname) {
+      throw new Error();
+    }
+
+    return resolvePath(decodeURI(urlObject.pathname), this.options).then(
+      (resolvedPath) =>
+        new Asset(
+          resolvedPath,
+          urlObject.search || "",
+          urlObject.hash || "",
+          this.options.basePath || ".",
+          this.options.baseURL || "/"
+        )
+    );
+  }
+}
+
+export default Assets;
